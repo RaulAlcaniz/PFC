2351fa57e63b0ff7b489e9c6068b055016f003a1
ralcaniz83@gmail.com <ralcaniz83@gmail.com>
2014-05-14 12:24:42 +0000

    Manage_sections acabada

--- app/models/section.rb
+++ app/models/section.rb
@@ -2,5 +2,5 @@ class Section < ActiveRecord::Base
   belongs_to :group
 
   validates :name, presence: true
-  validates :description, presence: true, uniqueness: {case_sensitive: false}
+  validates :description, presence: true
 end
