ff9881ca9f7654aac7c5d5eb1d79e8293e4fda26
ralcaniz83@gmail.com <ralcaniz83@gmail.com>
2015-01-26 23:08:42 +0000

    vistas

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
 
   # Devise option
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = false
-  config.action_mailer.default :charset => "utf-8"
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.perform_deliveries = false
+  # config.action_mailer.default :charset => "utf-8"
 
 end
